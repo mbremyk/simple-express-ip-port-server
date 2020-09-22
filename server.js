a91b2cc2caadab6d55ba74a84d66b545cabcfddd
Magnus Brevik <mbremyk@hotmail.no>
2020-09-22 21:26:23 +0200

    I try

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ let express = require("express");
 let cors = require('cors');
 let app = express();
 let fs = require('fs');
+let expressip = require('express-ip');
+app.use(expressip().getIpInfoMiddleware);
 app.use(cors());
 
 app.get("/", (req, res) => {
     fs.readFile('index.html', 'utf-8', (err, data) => {
         if (err) throw err;
         data = data.replace('ipnr', req.ip).replace('portnr', req.connection.remotePort);
-        res.send(data);
+        res.send(req.ipInfo);
     })
 });
 
