7e7331192c1b5695281f42b8f46997cf7158b893
Magnus Brevik <mbremyk@hotmail.no>
2020-09-22 21:38:05 +0200

    I try

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(cors());
 app.get("/", (req, res) => {
     fs.readFile('index.html', 'utf-8', (err, data) => {
         if (err) throw err;
-        data = data.replace('ipnr', req.ip).replace('portnr', req.connection.remotePort);
-        res.send(req.ipInfo);
+        data = data.replace('ipnr', req.ipInfo.ip).replace('portnr', req.connection.remotePort);
+        res.send(data);
     })
 });
 
